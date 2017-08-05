6e598899f93932f009b46c3f5274f2240b6b8b0e
davidandrus <davidandrus.com@gmail.com>
2017-08-05 10:05:43 -0700

    adding antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import 'antd/dist/antd.css';
+
 // @TODO - possibly replace with redux router
 import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+
+import { Layout } from 'antd';
 
 import {
   Root,
@@ -12,16 +16,22 @@ import {
   Post,
 } from './views';
 
+const { Header, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Route exact path="/" component={Root} />
-          <Route exact path="/category" component={Category} />
-          <Route exact path="/post" component={Post} />
-        </div>
+        <Layout>
+          <Header>
+            <div style={{background: 'red', height: 50, width: 100 }} />
+          </Header>
+          <Content>
+            <Route exact path="/" component={Root} />
+            <Route exact path="/category" component={Category} />
+            <Route exact path="/post" component={Post} />
+          </Content>
+        </Layout>
       </Router>
     );
   }
