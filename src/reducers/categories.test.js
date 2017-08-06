c4720eb9794696a4ff6e537c53872f3de0c71934
davidandrus <davidandrus.com@gmail.com>
2017-08-06 10:11:21 -0700

    breaking up reducers into smaller modules

---
+++ src/reducers/categories.test.js
@@ -0,0 +1,16 @@
+import categories from './categories';
+
+it('should initialize with empty array', () => {
+  const subject = categories();
+  expect(subject).toEqual([]);
+});
+
+it('should return payload when triggered with LOAD_CATEGORIES action', () => {
+  const results = [{ name: 'item 1'}];
+  const subject = categories([], {
+    type: 'LOAD_CATEGORIES',
+    payload: results,
+  });
+
+  expect(subject).toEqual(results);
+});
--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,13 +1,6 @@
-import { LOAD_CATEGORIES } from '../actions/actionNames';
-export default function(state = {categories: []}, action) {
-  switch(action.type) {
-    case LOAD_CATEGORIES:
-      console.log('dispatched', action.payload)
-      return {
-        ...state,
-        categories: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+import { combineReducers } from 'redux';
+import categories from './categories';
+
+export default combineReducers({
+  categories,
+});
